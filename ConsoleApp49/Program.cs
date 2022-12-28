b6c4fdf7792727dbfc5c96f97f692de40722f6ff
huseynManaf <huseynim@code.edu.az>
2022-12-28 01:18:32 -0800

    init

---
+++ ConsoleApp49/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp49
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin");
+            int.Parse(Console.ReadLine());  
+            int a = 12546;
+            Console.WriteLine("Muxtelifreqemli");
+        }
+    }
+}
